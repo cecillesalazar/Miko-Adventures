e07af4f87d0aa647703e9a872d2d08777be62866
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-08 08:20:33 -0700

    Added setup to preload background, trees, shrubs, bone, and Miko spritesheet

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,6 +1,18 @@
 export class SimpleScene extends Phaser.Scene {
+  
   preload() {
-    this.load.image('cokecan', 'assets/cokecan.png');
+    this.load.image('Background', 'https://s25.postimg.cc/fkh4tcpq7/Green-_Background.png');
+    this.load.image('Trees', 'assets/tree-apple-right.gif');
+    this.load.image('Shrubs', 'assets/shrubs.gif');
+    this.load.image('Bone', 'https://s25.postimg.cc/l3xgg2e4v/Dog_Bone.gif');
+    this.load.spritesheet(
+      'Miko',
+      'assets/miko-all-directions.png',
+      {
+        frameWidth: 64,
+        frameHeight: 64
+      }
+    )
   }
 
   create() {
