63eeb3e9c699c985d6a5fde31b2a911ce92ee8ad
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-08 08:00:44 -0700

    Pulled uploaded test  image file from github and added to assets folder

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,7 +1,7 @@
 export class SimpleScene extends Phaser.Scene {
-  // preload() {
-  //   this.load.image('cokecan', 'assets/cokecan.png');
-  // }
+  preload() {
+    this.load.image('cokecan', 'assets/cokecan.png');
+  }
 
   create() {
     this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
