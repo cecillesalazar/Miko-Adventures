6dc1c4d3790d254bf9e69f3b4fccbda92f651674
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-09 06:37:30 -0700

    Fixed file paths and added variables

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,13 +1,13 @@
 export class SimpleScene extends Phaser.Scene {
 
   preload() {
-    this.load.image('Background', 'assets/Green-Background.png');
-    this.load.image('Trees', 'assets/Tree-Apple-Right.gif');
-    this.load.image('Shrubs', 'assets/Shrub.gif');
-    this.load.image('Bone', 'assets/dog-bone.png');
+    this.load.image('Background', '../../assets/Green-Background.png');
+    this.load.image('Trees', '../../assets/Tree-Apple-Right.gif');
+    this.load.image('Shrubs', '../../assets/Shrub.gif');
+    this.load.image('Bone', '../../assets/dog-bone.png');
     this.load.spritesheet(
       'Miko',
-      'assets/miko-all-directions.png',
+      '../../assets/miko-all-directions.png',
       {
         frameWidth: 64,
         frameHeight: 64
@@ -16,7 +16,8 @@ export class SimpleScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    this.add.image(100, 200, 'cokecan');
+    // this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+    this.add.image(100, 200, 'Background');
+
   }
 }
