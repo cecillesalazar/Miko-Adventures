df5f47603004f1c7fe63b8e38937e386188b2cbb
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-09 06:45:40 -0700

    Added pixelArt enhancement and physics configurations in gameConfig

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const gameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  pixelArt: true,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+    }
+  },
   scene: SimpleScene
 };
 
